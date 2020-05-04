b7b79ce417fda8dbde3896ef0063d3405c5b5e65
Miles Higbie <miles.higbie@gmail.com>
2020-05-04 15:01:36 -0400

    changed img on request for demo

--- components/Header.js
+++ components/Header.js
@@ -7,15 +7,15 @@ const Header = (props) => {
       {props.face === true ? 
         <Image
         style= {styles.img}
-         source={{
-          uri: 'https://scontent.fewr1-6.fna.fbcdn.net/v/t1.0-9/578391_10151762481348753_128908989_n.jpg?_nc_cat=102&_nc_sid=43edb5&_nc_ohc=bw593kkojrsAX8NkVxJ&_nc_ht=scontent.fewr1-6.fna&oh=15dda8fc59c8f92ee4bf0a1f7abf7505&oe=5ED2145E'
+        source={{
+          uri: 'https://scontent.fewr1-5.fna.fbcdn.net/v/t1.0-9/15697817_10155596529823294_3274696557151618243_n.jpg?_nc_cat=107&_nc_sid=05277f&_nc_ohc=wZik3HE-P5UAX8P9otL&_nc_ht=scontent.fewr1-5.fna&oh=d422f7811b96b1a037cde2e97cbd62e0&oe=5ED668BF'
         }}
       />
       : 
       <Image
         style= {styles.img}
          source={{
-          uri: 'https://scontent.fewr1-5.fna.fbcdn.net/v/t31.0-8/1402216_10151866328188795_1272302535_o.jpg?_nc_cat=107&_nc_sid=e007fa&_nc_ohc=hp_3M-QfyZIAX9BTpSK&_nc_ht=scontent.fewr1-5.fna&oh=5d33b22e8240fad2f03c73f74b51a28a&oe=5ED360F9'
+          uri: "https://scontent.fewr1-2.fna.fbcdn.net/v/t1.0-9/65437496_10158522066788294_6668811976950415360_o.jpg?_nc_cat=111&_nc_sid=7aed08&_nc_ohc=jyOw7ZB2R7MAX_M9pb0&_nc_ht=scontent.fewr1-2.fna&oh=6302b0f13f8816a765de6328d23f27e8&oe=5ED55675"
         }}
       />
       } 
@@ -28,7 +28,7 @@ const Header = (props) => {
             {props.face === true ? 
             <Text style={styles.txt}> Click to take it away! </Text> 
             :
-            <Text style={styles.txt}> Click to Give Evan a Kiss! </Text>
+            <Text style={styles.txt}> Click to Give Cindy Tequila</Text>
             }
     </TouchableOpacity>
     </View>
